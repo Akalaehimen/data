096dbf250eee2aa42b335d907a3cf0931fd4d489
Akala Ehimen <ehimenakala45@gmail.com>
2022-10-06 20:56:39 -0700

    changes

--- app.py
+++ app.py
@@ -7,21 +7,22 @@ from re import S
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from flask_login import login_user, LoginManager, UserMixin, logout_user
 from sqlalchemy.sql import func
-
+ 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY']='72e8e98dd2e76fe548ea3372'
 
 db = SQLAlchemy(app)
 
-
+Login_manager = LoginManager(app)
 # ...
-class Student(db.Model):
+class Student(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(100), nullable=False)
     lastname = db.Column(db.String(100), nullable=False)
@@ -33,18 +34,22 @@ class Student(db.Model):
 
     def __repr__(self):
         return f'<Student {self.firstname}>'
+
+@Login_manager.user_loader
+def user_loader(id):
+    return Student.query.get(int(id))
     
 # ...
 @app.route('/')
 def index():
     #    student = Student.query.filter_by(email = email).first()
     return render_template('index.html')
-    # return '<h1>Welcome</h1
+    
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def register():
-    if request.method == 'post' :
+    if request.method == 'POST' :
         firstname = request.form.get('firstname')
         email = request.form.get('email')
         password = request.form.get('password')
@@ -77,13 +82,24 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    firstname = request.form.get('firstname')
+    password = request.form.get('password')
+    
+    student= Student.query.filter_by(firstname=firstname).first()
+
+    if Student and check_password_hash(Student.password_hash, password):
+        login_user(student)
+        return redirect(url_for('index'))
+
     return render_template('login.html')
 
 
 
 @app.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('index'))
+    
 
     
 if __name__=='__main__':
@@ -94,4 +110,3 @@ if __name__=='__main__':
 
 
 
-    
